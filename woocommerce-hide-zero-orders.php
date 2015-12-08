438a00b789209b9de702fd77b3f803a469fb7220
Daniel Espinoza <daniel@growdevelopment.com>
2015-12-08 12:05:27 -0600

    Initial commit.

---
+++ woocommerce-hide-zero-orders.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Hide Zero Orders
+ * Plugin URI: https://shopplugins.com
+ * Description: Adds a filter to the Edit Orders admin page to show all or non-zero orders. Handy to hide $0 orders.
+ * Version: 0.0.1
+ * Author: Shop Plugins
+ * Author URI: https://shopplugins.com
+ * Textdomain: sp-hide-zero-orders
+ */
+
+if ( is_admin() ) {
+	add_action( 'restrict_manage_posts', 'sp_restrict_orders', 50 );
+	function sp_restrict_orders() {
+		global $typenow;
+
+		if ( 'shop_order' != $typenow ) {
+			return;
+		}
+
+
+		?>
+		<select name='sp_order_view' id='dropdown_sp_order_view'>
+			<option <?php
+				if ( isset( $_GET['sp_order_view'] ) && $_GET['sp_order_view'] ) {
+					selected( 'all', $_GET['sp_order_view'] );
+				}
+			?> value="all"><?php esc_html_e( 'Show all orders', 'sp-hide-zero-orders' ); ?></option>
+			<option <?php
+			        if ( isset( $_GET['sp_order_view'] ) && $_GET['sp_order_view'] ) {
+				        selected( 'non-zero', $_GET['sp_order_view'] );
+			        }
+			        ?>value="non-zero"><?php esc_html_e( 'Show non-zero orders', 'sp-hide-zero-orders' ); ?></option>
+		</select>
+		<?php
+	}
+
+
+	add_filter( 'request', 'sp_orders_by_restrict_option' );
+	function sp_orders_by_restrict_option( $vars ) {
+		global $typenow;
+		$key = 'post__in';
+error_log("here 1");
+		if ( 'shop_order' == $typenow && isset( $_GET['sp_order_view'] ) ) {
+			error_log("here 2");
+			if ( 'non-zero' == $_GET['sp_order_view'] ) {
+				error_log("here 3");
+				if ( ! empty( $key ) ) {
+					error_log("here 4");
+					$vars[ $key ] = get_posts( array(
+						'posts_per_page' => -1,
+						'post_type'      => 'shop_order',
+						'post_status'    => 'any',
+						'fields'         => 'ids',
+						'orderby'        => 'date',
+						'order'          => 'DESC',
+						'meta_query'     => array(
+							array(
+								'key'     => '_order_total',
+								'value'   => 0,
+								'compare' => '>',
+							),
+						),
+					) );
+
+				}
+
+			}
+
+		}
+
+		return $vars;
+	}
+}
